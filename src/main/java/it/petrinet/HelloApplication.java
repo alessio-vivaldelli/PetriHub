57cc1d989f3d0f51f855e2d5d1aedd05f8fbb58a
alessio-vivaldelli <alessio99vivaldelli@gmail.com>
2025-05-09 18:45:03 +0200

    first graph test

--- src/main/java/it/petrinet/HelloApplication.java
+++ src/main/java/it/petrinet/HelloApplication.java
@@ -1,20 +1,71 @@
 package it.petrinet;
 
+import com.brunomnsilva.smartgraph.graph.Graph;
+import com.brunomnsilva.smartgraph.graph.GraphEdgeList;
+import com.brunomnsilva.smartgraph.graphview.SmartCircularSortedPlacementStrategy;
+import com.brunomnsilva.smartgraph.graphview.SmartGraphPanel;
+import com.brunomnsilva.smartgraph.graphview.SmartPlacementStrategy;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+// Create the graph
+        Graph<String, String> g = new GraphEdgeList<>();
+// ... see Examples below
+
+        SmartPlacementStrategy initialPlacement = new SmartCircularSortedPlacementStrategy();
+        SmartGraphPanel<String, String> graphView = new SmartGraphPanel<>(g, initialPlacement);
+        Scene scene = new Scene(graphView, 1024, 768);
+
+        stage = new Stage(StageStyle.DECORATED);
+        stage.setTitle("JavaFXGraph Visualization");
         stage.setScene(scene);
         stage.show();
+
+//IMPORTANT! - Called after scene is displayed, so we can initialize the graph visualization
+        graphView.init();
+
+        g.insertVertex("A");
+        g.insertVertex("B");
+        g.insertVertex("C");
+        g.insertVertex("D");
+        g.insertVertex("E");
+        g.insertVertex("F");
+        g.insertVertex("G");
+
+        g.insertEdge("A", "B", "1");
+        g.insertEdge("A", "C", "2");
+        g.insertEdge("A", "D", "3");
+        g.insertEdge("A", "E", "4");
+        g.insertEdge("A", "F", "5");
+        g.insertEdge("A", "G", "6");
+
+        g.insertVertex("H");
+        g.insertVertex("I");
+        g.insertVertex("J");
+        g.insertVertex("K");
+        g.insertVertex("L");
+        g.insertVertex("M");
+        g.insertVertex("N");
+
+        g.insertEdge("H", "I", "7");
+        g.insertEdge("H", "J", "8");
+        g.insertEdge("H", "K", "9");
+        g.insertEdge("H", "L", "10");
+        g.insertEdge("H", "M", "11");
+        g.insertEdge("H", "N", "12");
+
+        g.insertEdge("A", "H", "0");
+
+        graphView.setAutomaticLayout(true);
+        graphView.update();
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module it.petrinet {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires smartgraph;
 
     opens it.petrinet to javafx.fxml;
     exports it.petrinet;
