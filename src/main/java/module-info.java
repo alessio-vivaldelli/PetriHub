2dfcf940f8aea2bcfd35e2cb166f47922150bdac
alessio-vivaldelli <alessio99vivaldelli@gmail.com>
2025-05-21 14:02:46 +0200

    minor changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,17 @@
 module it.petrinet {
-    requires javafx.fxml;
+  requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-//    requires smartgraph;
-//    requires javafx.fxml;
-    requires java.logging;
-    requires jdk.xml.dom;
-    requires atlantafx.base;
-    opens it.petrinet to javafx.fxml;
-    exports it.petrinet;
+  requires org.controlsfx.controls;
+  // requires smartgraph;
+  // requires javafx.fxml;
+  requires java.logging;
+  requires jdk.xml.dom;
+  requires atlantafx.base;
+
+  opens it.petrinet to javafx.fxml;
+  opens it.petrinet.controller to javafx.fxml;
+
+  exports it.petrinet.model;
+  exports it.petrinet.controller;
+  exports it.petrinet;
 }
